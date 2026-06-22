b0b42a6feeefe3c07536cedc7775eaa57a14ad17
ismaelnata01-maker <ismaelnata01@gmail.com>
2026-06-22 17:18:37 -0300

    Add files via upload

---
+++ app/index.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Home(){
+    return(
+        <SafeAreaView style={styles.screen}>
+            <Text style={styles.title}>Flappy Bird</Text>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    screen:{
+        width: "100%",
+        height: "100%",
+        backgroundColor: "black",
+        alignItems: "center",
+    },
+    title:{
+        fontSize: 50,
+        fontWeight: "bold",
+        color:"white",
+        marginTop: 100,
+    },
+})
